b1b79c381aa44a696fe1454c58b28da81cfa9ab2
maulanaalifian2 <maulanaalifian2@gmail.com>
2026-05-12 18:00:04 +0700

    Delete RWD directory

--- RWD/sidebar.php
+++
@@ -1,7 +0,0 @@
-<ul class="list-group">
-  <li class="list-group-item active" aria-current="true">Studies Category</li>
-  <li class="list-group-item">SD</li>
-  <li class="list-group-item">SMP</li>
-  <li class="list-group-item">SMK</li>
-  <li class="list-group-item">Perguruan Tinggi</li>
-</ul>
